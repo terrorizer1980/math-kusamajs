14e2d3604e51cf003f5036366ce440963343171a
Eric Yu <ericfish@gmail.com>
2020-03-11 16:15:46 +0800

    add remark basic func

--- index.js
+++ index.js
@@ -73,6 +73,36 @@ class PolkadotWeb3JSSample {
 
 		return h;
 	}
+
+	/***
+	 * Remark
+	 * @param from from
+	 * @param content content
+	 * @return hash
+	 */
+	async remark(from, content) {
+
+		//////////////////////////// alexander  ////////////////////////////
+		// Initialise the provider to connect to the local node
+		const provider = new WsProvider('wss://kusama-rpc.polkadot.io');
+		// Create the API and wait until ready
+		const api = await ApiPromise.create({ provider });
+
+		// finds an injector for an address
+		const injector = await web3FromAddress(from);
+
+		// sets the signer for the address on the @polkadot/api
+		api.setSigner(injector.signer);
+
+		// sign and send out transaction - notice here that the address of the account (as retrieved injected)
+		// is passed through as the param to the `signAndSend`, the API then calls the extension to present
+		// to the user and get it signed. Once completex, the api sends the tx + signature via the normal process
+		const h = api.tx.system
+			.remark(content)
+			.signAndSend(from);
+
+		return h;
+	}
 }
 
 window.PolkadotWeb3JSSample = new PolkadotWeb3JSSample();
